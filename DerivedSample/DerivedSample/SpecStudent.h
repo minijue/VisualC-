c0de584ec8188bfe5e2ddbc721799f7ab7d66385
王珏 <jue952@live.cn>
2017-03-30 16:37:18 +0800

    课堂示例，继承

---
+++ DerivedSample/DerivedSample/SpecStudent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Student.h"
+class SpecStudent :
+	public Student {
+public:
+	SpecStudent();
+	~SpecStudent();
+
+	void display() const;
+	void modifyAdditionalScore(double as);
+
+protected:
+	double aScore{ 0.0 };
+	double calScore() const;
+};
+
